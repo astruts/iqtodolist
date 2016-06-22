edccc8e33de2909ad67c87f9b18ebef91546ef8e
Yukon <Yukon@Olegs-Mac-mini.local>
2016-06-22 15:36:02 +0300

    new class IQToD0Item

---
+++ IQToDoList/IQToDoItem.h
@@ -0,0 +1,13 @@
+//
+//  IQToDoItem.h
+//  IQToDoList
+//
+//  Created by Yukon on 6/22/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IQToDoItem : NSObject
+
+@end
---
+++ IQToDoList/IQToDoItem.m
@@ -0,0 +1,13 @@
+//
+//  IQToDoItem.m
+//  IQToDoList
+//
+//  Created by Yukon on 6/22/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import "IQToDoItem.h"
+
+@implementation IQToDoItem
+
+@end
