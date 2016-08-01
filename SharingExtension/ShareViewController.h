02ba6f206b91c9b989f4230d346444e7f2a47979
Andrii Struts <astruts@iqmessenger.com>
2016-08-01 13:12:25 +0300

    Added sample sharing extension

---
+++ SharingExtension/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  SharingExtension
+//
+//  Created by Andrii Struts on 8/1/16.
+//  Copyright © 2016 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
